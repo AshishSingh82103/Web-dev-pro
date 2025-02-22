3de201d6fbf09a11eb6a8ea044bb036fd2ec8a4c
Ashish Singh <singhashish82103@gmail.com>
2025-02-22 12:58:07 +0530

    Initial commit

--- 04_Opps_javaScript/01_prototypes.js
+++ 04_Opps_javaScript/01_prototypes.js
@@ -1,3 +1,39 @@
 /*Prototypes => it means get some extra properties and extra functionallty in any given data types 
 and everything in javascript is called object
 */
+
+let computer = {cpu: 12}
+
+let lenvo = {
+    screen: "HD",
+    __proto__: computer,
+
+}
+
+let tomHardware = {}
+
+// console.log(`computer`, computer.__proto__);
+
+// console.log(`lenvo`, lenvo.__proto__);
+
+
+
+let genricCar = {tyers: 4}
+
+let tesla = {
+    driver: "AI"
+}
+
+Object.setPrototypeOf(tesla, genricCar);
+
+console.log(`tesla `,tesla.tyers);
+console.log(`tesla `, Object.getPrototypeOf(tesla));
+
+
+
+
+
+
+  
+
+
