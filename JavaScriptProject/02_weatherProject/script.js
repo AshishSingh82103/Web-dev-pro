cc30a3c0b0cca57ea2956e896bf030278cefa48f
Ashish Singh <singhashish82103@gmail.com>
2025-03-05 20:48:22 +0530

    Initial commit

--- JavaScriptProject/02_weatherProject/script.js
+++ JavaScriptProject/02_weatherProject/script.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   
+    
     async function fetchWeatherData(city) {
       //gets the data
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
